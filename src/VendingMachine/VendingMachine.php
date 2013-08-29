a3ad5ba17e39cb91db4f34ebae908d5acbe435e9
patorash <chariderpato@gmail.com>
2013-08-29 18:33:40 +0900

    素振り中。

--- src/VendingMachine/VendingMachine.php
+++ src/VendingMachine/VendingMachine.php
@@ -12,12 +12,32 @@ namespace VendingMachine;
 
 class VendingMachine {
     private $total = 0;
+
     public function getTotal() {
         return $this->total;
     }
 
-    public function putMoney($coin) {
-        $this->total += $coin;
-        return true;
+    public function putMoney($money) {
+        switch($money) {
+            case 10:
+            case 50:
+            case 100:
+            case 500:
+            case 1000:
+                $this->total += $money;
+                return true;
+
+            default:
+                $this->showChange($money);
+                return false;
+        }
+    }
+
+    public function returnChange() {
+        $this->showChange($this->getTotal());
+    }
+
+    public function showChange($money) {
+        echo "お釣り: {$money}円";
     }
 }
--- tests/VendingMachine/VendingMachineTest.php
+++ tests/VendingMachine/VendingMachineTest.php
@@ -23,4 +23,67 @@ class VendingMachineTest extends \PHPUnit_Framework_TestCase {
             $this->assertTrue($this->vendingMachine->putMoney($money));
         }
     }
+
+    /**
+     * @test
+     */
+    public function 投入金額の総計を取得できる() {
+        $moneys = [10, 50, 100, 500, 1000];
+        foreach($moneys as $money) {
+            $this->vendingMachine->putMoney($money);
+        }
+        $this->assertEquals(1660, $this->vendingMachine->getTotal());
+    }
+
+    /**
+     * @test
+     */
+    public function 払い戻し動作を行うと投入金額の総計を釣り銭として出力する() {
+        $moneys = [10, 50, 100, 500, 1000];
+        foreach($moneys as $money) {
+            $this->vendingMachine->putMoney($money);
+        }
+        $this->expectOutputString("お釣り: 1660円");
+        $this->vendingMachine->returnChange();
+    }
+
+    public function putInvalidMoney($invalidMoney){
+        $this->expectOutputString("お釣り: {$invalidMoney}円");
+        $this->vendingMachine->putMoney($invalidMoney);
+    }
+
+    /**
+     * @test
+     */
+    public function 扱えないお金1円が投入された場合は釣り銭としてユーザに出力する() {
+        $this->putInvalidMoney(1);
+    }
+
+    /**
+     * @test
+     */
+    public function 扱えないお金5円が投入された場合は釣り銭としてユーザに出力する() {
+        $this->putInvalidMoney(5);
+    }
+
+    /**
+     * @test
+     */
+    public function 扱えないお金2000円が投入された場合は釣り銭としてユーザに出力する() {
+        $this->putInvalidMoney(2000);
+    }
+
+    /**
+     * @test
+     */
+    public function 扱えないお金5000円が投入された場合は釣り銭としてユーザに出力する() {
+        $this->putInvalidMoney(5000);
+    }
+
+    /**
+     * @test
+     */
+    public function 扱えないお金10000円が投入された場合は釣り銭としてユーザに出力する() {
+        $this->putInvalidMoney(10000);
+    }
 }
